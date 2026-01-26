0215aafe0f8bca8cc30a43989ec6fce1c57de4db
Rudy Dupuis <106442315+RudyDupuis@users.noreply.github.com>
2026-01-26 09:17:58 +0100

    Enhance SvgDisplayer with skill names

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -152,15 +152,18 @@ const filteredHardSkills = computed<Array<HardSkill>>(() => {
         <div
           class="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-10 gap-4"
         >
-          <SvgDisplayer
-            v-for="(hardSkill, index) in filteredHardSkills"
-            :key="index"
-            :svg="hardSkill.svg"
-            :size="60"
-            :class="
-              hardSkill.mastery === 'advanced' ? 'fill-primary' : 'fill-grey-1'
-            "
-          />
+          <div class="flex flex-col items-center">
+            <SvgDisplayer
+              v-for="(hardSkill, index) in filteredHardSkills"
+              :key="index"
+              :svg="hardSkill.svg"
+              :size="60"
+              :class="
+                hardSkill.mastery === 'advanced' ? 'fill-primary' : 'fill-grey-1'
+              "
+            />
+            <span class="text-sm">{{ hardSkill.name }}</span>
+          </div>
         </div>
         <FetchDataDisplayer
           :is-loading="hardskillIsLoading"
@@ -171,13 +174,16 @@ const filteredHardSkills = computed<Array<HardSkill>>(() => {
         <div
           class="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-10 gap-4"
         >
-          <SvgDisplayer
-            v-for="(softSkill, index) in softSkills"
-            :key="index"
-            :svg="softSkill.svg"
-            :size="60"
-            class="fill-grey-1"
-          />
+          <div class="flex flex-col items-center">
+            <SvgDisplayer
+              v-for="(softSkill, index) in softSkills"
+              :key="index"
+              :svg="softSkill.svg"
+              :size="60"
+              class="fill-grey-1"
+            />
+            <span class="text-sm">{{ softSkill.name }}</span>
+          </div>
         </div>
         <FetchDataDisplayer
           :is-loading="softskillIsLoading"
